d539fdfb5603abfd23c42f7a80bac50758f69d97
troje888 <jordyb25@msn.com>
2015-04-08 11:23:53 +0200

    Strategy Pattern

---
+++ chapter-7/StrategyPattern/StrategyPattern/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace StrategyPattern
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bubbleSort = new System.Windows.Forms.Button();
+            this.insertionSort = new System.Windows.Forms.Button();
+            this.selectionSort = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.newArray = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bubbleSort
+            // 
+            this.bubbleSort.Location = new System.Drawing.Point(12, 12);
+            this.bubbleSort.Name = "bubbleSort";
+            this.bubbleSort.Size = new System.Drawing.Size(85, 23);
+            this.bubbleSort.TabIndex = 0;
+            this.bubbleSort.Text = "BubbleSort";
+            this.bubbleSort.UseVisualStyleBackColor = true;
+            this.bubbleSort.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // insertionSort
+            // 
+            this.insertionSort.Location = new System.Drawing.Point(103, 12);
+            this.insertionSort.Name = "insertionSort";
+            this.insertionSort.Size = new System.Drawing.Size(85, 23);
+            this.insertionSort.TabIndex = 1;
+            this.insertionSort.Text = "InsertionSort";
+            this.insertionSort.UseVisualStyleBackColor = true;
+            this.insertionSort.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // selectionSort
+            // 
+            this.selectionSort.Location = new System.Drawing.Point(194, 12);
+            this.selectionSort.Name = "selectionSort";
+            this.selectionSort.Size = new System.Drawing.Size(85, 23);
+            this.selectionSort.TabIndex = 2;
+            this.selectionSort.Text = "SelectionSort";
+            this.selectionSort.UseVisualStyleBackColor = true;
+            this.selectionSort.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 277);
+            this.listBox1.TabIndex = 3;
+            // 
+            // newArray
+            // 
+            this.newArray.Location = new System.Drawing.Point(285, 12);
+            this.newArray.Name = "newArray";
+            this.newArray.Size = new System.Drawing.Size(85, 23);
+            this.newArray.TabIndex = 4;
+            this.newArray.Text = "New";
+            this.newArray.UseVisualStyleBackColor = true;
+            this.newArray.Click += new System.EventHandler(this.newArray_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 335);
+            this.Controls.Add(this.newArray);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.selectionSort);
+            this.Controls.Add(this.insertionSort);
+            this.Controls.Add(this.bubbleSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bubbleSort;
+        private System.Windows.Forms.Button insertionSort;
+        private System.Windows.Forms.Button selectionSort;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button newArray;
+    }
+}
+
---
+++ chapter-7/StrategyPattern/StrategyPattern/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StrategyPattern
+{
+    public partial class Form1 : Form
+    {
+        int[] randomArray;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            randomArray = new int[10];
+            createRandomArray();
+            printArray("New random array:", randomArray);
+        }
+
+        //Create a random array
+        public void createRandomArray()
+        {
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = random.Next(randomArray.Length * 2);
+            }
+        }
+
+        //Print an array to the ListBox
+        public void printArray(string title, int[] array)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add(title);
+            for (int i = 0; i < array.Length; i++)
+            {
+                listBox1.Items.Add(array[i].ToString());
+            }
+            listBox1.Items.Add("");
+        }
+
+        private void newArray_Click(object sender, EventArgs e)
+        {
+            createRandomArray();
+            printArray("New random array:", randomArray);
+        }
+
+        //The Context
+        void ButtonClick(object sender, EventArgs e)
+        {
+            Button control = sender as Button;
+            SortStrategy strategy = null;
+
+            switch (control.Name)
+            {
+                case "bubbleSort":
+                    strategy = new BubbleSort();
+                    break;
+                case "insertionSort":
+                    strategy = new InsertionSort();
+                    break;
+                case "selectionSort":
+                    strategy = new SelectionSort();
+                    break;
+            }
+
+            int[] sortedArray = strategy.Sort(randomArray);
+            printArray(strategy.GetType() + " results:", sortedArray);
+        }
+    }
+
+    //Strategy Interface
+    interface SortStrategy
+    {
+        int[] Sort(int[] array);
+    }
+
+    //Strategy 1
+    class BubbleSort : SortStrategy
+    {
+        public int[] Sort(int[] array)
+        {
+            int[] newArray = (int[])array.Clone();
+            for (int outer = newArray.Length - 1; outer > 0; outer--)
+            {
+                for (int inner = 0; inner < outer; inner++)
+                {
+                    if (newArray[inner].CompareTo(newArray[inner + 1]) > 0)
+                    {
+                        int temp = newArray[inner];
+                        newArray[inner] = newArray[inner + 1];
+                        newArray[inner + 1] = temp;
+                    }
+                }
+            }
+            return newArray;
+        }
+    }
+
+    //Strategy 2
+    class InsertionSort : SortStrategy
+    {
+        public int[] Sort(int[] array)
+        {
+            int[] newArray = (int[])array.Clone();
+            int inner;
+            for (int outer = 1; outer < newArray.Length; outer++)
+            {
+                int temp = newArray[outer];
+                inner = outer;
+                while (inner > 0 && newArray[inner - 1].CompareTo(temp) >= 0)
+                {
+                    newArray[inner] = newArray[inner - 1];
+                    inner--;
+                }
+                newArray[inner] = temp;
+            }
+            return newArray;
+        }
+    }
+
+    //Strategy 3
+    class SelectionSort : SortStrategy
+    {
+        public int[] Sort(int[] array)
+        {
+            int[] newArray = (int[])array.Clone();
+            int min;
+            for (int outer = 0; outer < newArray.Length; outer++)
+            {
+                min = outer;
+                for (int inner = outer + 1; inner < newArray.Length; inner++)
+                    if (newArray[inner].CompareTo(newArray[min]) < 0)
+                        min = inner;
+                int temp = newArray[outer];
+                newArray[outer] = newArray[min];
+                newArray[min] = temp;
+            }
+            return newArray;
+        }
+    }
+}
